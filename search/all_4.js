0a88d05556686dadc55d120c9b7115efbcab435f
junamat <junamat@users.noreply.github.com>
2026-02-15 10:49:17 +0000

    deploy: e8aebb3a5824f1362829c12df6ef815e64348b36

--- search/all_4.js
+++ search/all_4.js
@@ -5,5 +5,6 @@ var searchData=
   ['endrefasync_2',['EndRefAsync',['../classSlashCommandManager.html#ac866fe33be7a8ba16a350ae58f428462',1,'SlashCommandManager']]],
   ['endtime_3',['endtime',['../classModels_1_1MatchRoom.html#a1b4069c744fb1ab7ad607ce4a8574b74',1,'Models::MatchRoom::EndTime'],['../Models_8cs.html#abb5643b174907596b4b306c669ca815e',1,'EndTime:&#160;Models.cs']]],
   ['envvarname_4',['envVarName',['../classRequireFromEnvIdAttribute.html#ac4d59aa608017e68d7d71e21721492bf',1,'RequireFromEnvIdAttribute']]],
-  ['executeadmincommand_5',['executeadmincommand',['../classAutoRefEliminationStage.html#a757ebb4fe140388b81cf9f491278d5d0',1,'AutoRefEliminationStage::ExecuteAdminCommand()'],['../classAutoRefQualifiersStage.html#abd3729b8636cead6c4deb7219c19e140',1,'AutoRefQualifiersStage::ExecuteAdminCommand()']]]
+  ['er_20diagram_5',['Database Schema (ER Diagram)',['../classModels.html#autotoc_md2',1,'']]],
+  ['executeadmincommand_6',['executeadmincommand',['../classAutoRefEliminationStage.html#a757ebb4fe140388b81cf9f491278d5d0',1,'AutoRefEliminationStage::ExecuteAdminCommand()'],['../classAutoRefQualifiersStage.html#abd3729b8636cead6c4deb7219c19e140',1,'AutoRefQualifiersStage::ExecuteAdminCommand()']]]
 ];
